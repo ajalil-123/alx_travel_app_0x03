e1d9b92306884494178701128dca599eaabcab74
Jalil <abduljalilzakaria1@gmail.com>
2025-08-03 14:49:21 +0200

    added viewsets to the listing app

--- listings/views.py
+++ listings/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Listing, Booking
+from .serializers import ListingSerializer, BookingSerializer
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-# Create your views here.
+
+
+class ListingViewSet(viewsets.ModelViewSet):
+    queryset = Listing.objects.all()
+    serializer_class = ListingSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
